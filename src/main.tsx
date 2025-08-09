04b9557bdac535946a56b554397b8980689d7a7c
7efnawi <7efnaw.ii@gmail.com>
2025-08-09 20:07:25 +0300

    fix error2

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from 'next-themes'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import App from './App.tsx'
 import './index.css'
+
+// Inject Vercel Analytics & Speed Insights (non-React APIs)
+inject()
+injectSpeedInsights()
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider defaultTheme="dark" attribute="class" enableSystem={false}>
     <App />
-    <Analytics />
-    <SpeedInsights />
   </ThemeProvider>
 );
